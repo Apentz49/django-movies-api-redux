893201f7be082644f60a738e464778122fb8078a
Aaron Pentz <apentz49@gmail.com>
2015-08-20 07:00:24 -0700

    tests

--- crud/api/tests.py
+++ crud/api/tests.py
@@ -1,3 +1,37 @@
+from django.contrib.auth.models import User
 from django.test import TestCase
+from rest_framework import response
+from rest_framework.test import APIRequestFactory, APITestCase
+from api.views import ListCreateView
+from movie.models import Movie
 
-# Create your tests here.
+
+class ListViewTests(APITestCase):
+
+    def setUp(self):
+        user = User.objects.create(username='bob', email='a@a.com', password='a')
+        self.top_gun = Movie.objects.create(title="Top gun", owner=User.objects.get(pk=1))
+        self.fast_times = Movie.objects.create(title="fast times", owner=User.objects.get(pk=1))
+        self.stripes = Movie.objects.create(title="stripes", owner=User.objects.get(pk=1))
+
+    # def testPost(self):
+    #     factory = APIRequestFactory()
+    #     request = factory.post('/movies/', {'title': 'fast times', 'owner': 'user'}, format='json')
+    #     view = ListCreateView.as_view()
+    #     response = view(request)
+    #     self.assertEquals(response.content, {'title': 'fast times', 'owner': 'user'})
+
+    def testGet(self):
+        factory = APIRequestFactory()
+        request = factory.get('/movie/1/')
+        view = ListCreateView.as_view()
+        response = view(request)
+        response.render()
+        self.assertContains(response, "Top gun")
+
+#
+# class DeleteViewTests(TestCase):
+#
+#     def testUpdate(self):
+#         factory = APIRequestFactory()
+#         request =
--- crud/crud/settings.py
+++ crud/crud/settings.py
@@ -41,6 +41,9 @@ INSTALLED_APPS = (
     'rest_framework.authtoken',
     'movie',
     'api',
+    'django_extensions',
+    'debug_toolbar',
+
 )
 
 MIDDLEWARE_CLASSES = (
